a4e939b50a231f58c2ee4ea06f8a134160c47087
Morgen <anchroom@gmail.com>
2019-05-11 22:45:30 +0200

    after test

--- Hausaufgaben/H2-1.hs
+++ Hausaufgaben/H2-1.hs
@@ -20,15 +20,22 @@ sort3 (a, b, c)
     | (b == c) && (b < a) = (b, c, a)
     | otherwise = (a, b, c)
 
-sort2' :: [Int] -> [Int]
-sort2' [x : xs] = if (xs < x) then xs : x : [] else x : xs : []
-{-
-sort3' :: [Int] -> [Int]
-sort3' [_ : dritteNum] = sort2' [ersteNum] :sort2'(zweiteNum : dritteNum : [])
-
+first3Num :: [Int] -> [Int]
+first3Num (x: y: z: _) = [x, y, z]
 
 sortFirst3 :: [Int] -> [Int]
 sortFirst3 [] = []
-sortFirst3 [_] = [_]
-sortFirst3 [_ : _] = sort2'(_, _)
--}
+sortFirst3 [a] = [a]
+sortFirst3 [a , b] = if (b < a) then [b , a ] else [ a , b ]
+sortFirst3 [a, b, c]
+    | (a < c)  && (c < b) = [a, c, b]
+    | (b < a)  && (a < c) = [b, a, c]
+    | (b < c)  && (c < a) = [b, c, a]
+    | (a > b)  && (b > c) = [c, b, a]
+    | (c < a)  && (a < b) = [c, a, b]
+    | (a == c) && (a < b) = [a, c, b]
+    | (a == c) && (a > b) = [b, a, c]
+    | (a == b) && (c < a) = [c, a, b]
+    | (b == c) && (b < a) = [b, c, a]
+    | otherwise = [a, b, c]
+sortFirst3 (a: b: c: xs) = sortFirst3 [a, b, c] ++ [y | y <- xs]

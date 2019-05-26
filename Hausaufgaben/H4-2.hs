601498b8ebacf322c4b94bc5843a2d88b423664d
Morgen <anchroom@gmail.com>
2019-05-26 16:41:51 +0200

    waiting optimieren

--- Hausaufgaben/H4-2.hs
+++ Hausaufgaben/H4-2.hs
@@ -1,13 +1,14 @@
 -- | a) einen String in eine Liste aufeinanderfolgender
 --Textabschnitte und Platzhalter zerlege
+{-
 data Token = Text String
            | Placeholder String
              deriving (Eq, Show)
 
 tokenize :: String -> [Token]
 tokenize [] = []
-tokenize [t] = [ Text (t:[])]
-tokenize ("{": (x: xs): '}':[]) = [ Placeholder (x:xs)]
+tokenize ('{':x:xs) = [ Placeholder (x:xs)]
+tokenize (x: xs) = [ Text (x:xs)]
 otherwise = error "TODO: Implementierung vervollstaendigen"
 
 
@@ -18,3 +19,22 @@ otherwise = error "TODO: Implementierung vervollstaendigen"
 -- | b)
 --replace :: [Token] -> [(String, String)] -> String
 --replace x m = error "TODO: Implementierung vervollstaendigen"
+
+-}
+data Token = Text String
+           | Placeholder String
+             deriving (Eq, Show)
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize xs = parseText "" xs
+
+parseText :: String -> String -> [Token]
+parseText text ('{':xs) = Text text : parsePlaceholder "" xs
+parseText text (x:xs)   = parseText (text ++ [x]) xs
+parseText text ""       = [Text text]
+
+parsePlaceholder :: String -> String -> [Token]
+parsePlaceholder name ('}':xs) = Placeholder name : parseText "" xs
+parsePlaceholder name (x:xs)   = parsePlaceholder (name ++ [x]) xs
+parsePlaceholder name ""       = [Text name]

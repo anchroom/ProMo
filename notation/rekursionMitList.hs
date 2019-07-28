bec4b313b4cfdafa1262c89448460ad9b323e758
Morgen <anchroom@gmail.com>
2019-07-28 23:47:27 +0200

    wiederholen 28.07

---
+++ notation/rekursionMitList.hs
@@ -0,0 +1,20 @@
+--in Folie 3.28-31
+
+-- | length, to count how many elements a list have
+
+
+
+
+-- | reverse
+
+
+
+
+
+-- | insert 按照顺序插入
+
+
+-- | sort 运用insert的sort,就是插入排序算法
+
+
+-- | zip 把两个数组合并为一个
